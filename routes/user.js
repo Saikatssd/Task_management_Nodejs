7394652eae9b860eb51aba4ad50d1af624d5649e
Saikat Sundar Das <saikatsundardas@gmail.com>
2023-09-13 19:34:41 +0530

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,49 @@
+import express from "express";
+import { getMyProfile, login, logout, register } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+router.get("/me", isAuthenticated, getMyProfile);
+
+export default router;
+
+
+
+// import express from "express";
+// import { User } from "../models/user.js"
+// // import { deleteUser, getAllUsers, getUserDetails, login, register, updateUser } from "../controllers/user.js";
+// import { login, register, getMyProfile, logout } from "../controllers/user.js";
+// import { isAuthenticated } from "../middlewares/auth.js";
+
+// const router = express.Router();
+
+// // router.get("/all", getAllUsers);
+
+// router.post("/new", register);
+
+// router.post("/login", login);
+
+// router.get("/logout", logout);
+
+// // router.route("/userid/:id").get(getMyProfile);
+
+// router.get("/me", isAuthenticated, getMyProfile);
+// // .put(updateUser)
+// // .delete(deleteUser);
+
+// // router.get("/userid/:id", getUserDetails);
+
+// // router.put("/userid/:id",updateUser );
+
+// // router.delete("/userid/:id",deleteUser );
+
+// export default router;
+
+
+
